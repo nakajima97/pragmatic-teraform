0ad2d448ad0264097ba62d951adad72ecd1a3517
Yudai Nakajima <kituneudon97@gmail.com>
2023-03-13 21:27:15 +0900

    7章まで終えた

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
 variable "aws_account_id" {
-  
+
 }
 
 resource "aws_s3_bucket" "private" {
-  bucket = "private-pragmatic-terraform-${var.aws_account_id}"
+  bucket        = "private-pragmatic-terraform-${var.aws_account_id}"
   force_destroy = true
 }
 
 resource "aws_kms_key" "mykey" {
-  description = "This key is used to encrypt bucket objects"
+  description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
 }
 
@@ -25,21 +25,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.mykey.id
-      sse_algorithm = "aws:kms"
+      sse_algorithm     = "aws:kms"
     }
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "private" {
-  bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "alb_log" {
-  bucket = "alb-log-pragmatic-terraform-${var.aws_account_id}"
+  bucket        = "alb-log-pragmatic-terraform-${var.aws_account_id}"
   force_destroy = true
 }
 
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
   bucket = aws_s3_bucket.alb_log.id
 
   rule {
-    id = "rule-1"
+    id     = "rule-1"
     status = "Enabled"
 
     expiration {
@@ -58,12 +58,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
 
 data "aws_iam_policy_document" "alb_log" {
   statement {
-    effect = "Allow"
-    actions = ["S3:PutObject"]
+    effect    = "Allow"
+    actions   = ["S3:PutObject"]
     resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = [var.aws_account_id]
     }
   }
---
+++ vpc--subnet.tf
@@ -0,0 +1,127 @@
+resource "aws_vpc" "example" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "example"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public_1" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+}
+
+resource "aws_route_table" "public_2" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_1.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_1.id
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.65.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.private_2.id
+  }
+}
+
+resource "aws_route_table" "private_2" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.private_2.id
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_2.id
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_eip" "nat_gateway_2" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "private_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.private_1.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "private_2" {
+  allocation_id = aws_eip.nat_gateway_2.id
+  subnet_id     = aws_subnet.private_2.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
