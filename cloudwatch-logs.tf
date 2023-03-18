2b99ae03d2dbb68e412441f38a8c58969c9567e8
Yudai Nakajima <kituneudon97@gmail.com>
2023-03-18 14:25:59 +0900

    ecsでバッチを作成

--- cloudwatch-logs.tf
+++ cloudwatch-logs.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name = "/ecs/example"
   retention_in_days = 1
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name = "/ecs-scheduled-tasks/example"
+  retention_in_days = 1
+}
--- ecs.tf
+++ ecs.tf
@@ -49,3 +49,13 @@ module "nginx_sg" {
   port = 80
   cidr_blocks = [aws_vpc.example.cidr_block]
 }
+
+resource "aws_ecs_task_definition" "example_batch" {
+  family = "example-batch"
+  cpu = "256"
+  memory = "512"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = file("./batch_container_definitions.json")
+  execution_role_arn = module.ecs_task_execution_role.iam_role_arn
+}
