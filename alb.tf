fd282713f303f0b20b6cf282bad345ff013e1b8f
Yudai Nakajima <kituneudon97@gmail.com>
2023-03-18 12:27:52 +0900

    albの作成

--- alb.tf
+++ alb.tf
@@ -27,7 +27,9 @@ resource "aws_lb" "example" {
   load_balancer_type = "application"
   internal = false
   idle_timeout = 60
-  enable_deletion_protection = true
+  # 勉強用のため削除保護は無効化
+  enable_deletion_protection = false
+  # enable_deletion_protection = true
 
   subnets = [
     aws_subnet.public_0.id,
@@ -54,6 +56,62 @@ resource "aws_lb_listener" "http" {
       message_body = "これは「HTTP」です"
       status_code = "200"
     }
+  }
+}
+
+# ACM必須で独自ドメインも必要なのでコメントアウト
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.example.arn
+#   port = 443
+#   protocol = "HTTPS"
+#   # certificate_arn = aws_acm_certificate.example.arn
+#   ssl_policy = "ELBSecurityPolicy-2016-08"
+
+#   default_action {
+#     type = "fixed-response"
+
+#     fixed_response {
+#       content_type = "text/plain"
+#       message_body = "これは「HTTPS」です"
+#       status_code = 200
+#     }
+#   }
+# }
+
+resource "aws_lb_target_group" "example" {
+  name = "example"
+  target_type = "ip"
+  vpc_id = aws_vpc.example.id
+  port = 80
+  protocol = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path = "/"
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+    matcher = 200
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+
+  depends_on = [aws_lb.example]
+}
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    field = "path-pattern"
+    values = ["/*"]
   }
 }
 
