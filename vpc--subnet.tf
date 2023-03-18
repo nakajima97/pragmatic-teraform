78d1bb1114ad8688194e0c5a6a2e98f10bd7ec91
Yudai Nakajima <kituneudon97@gmail.com>
2023-03-18 11:26:59 +0900

    nameが書籍と違っていたので修正

--- vpc--subnet.tf
+++ vpc--subnet.tf
@@ -8,14 +8,14 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "public_1" {
+resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.example.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
 }
 
-resource "aws_subnet" "public_2" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.example.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
@@ -24,6 +24,15 @@ resource "aws_subnet" "public_2" {
 
 resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public_0" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
 }
 
 resource "aws_route_table" "public_1" {
@@ -35,22 +44,13 @@ resource "aws_route_table" "public_1" {
   }
 }
 
-resource "aws_route_table" "public_2" {
-  vpc_id = aws_vpc.example.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
-  }
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public_0.id
 }
 
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public_1.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_1.id
 }
 
